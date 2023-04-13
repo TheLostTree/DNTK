a9a155eb8a680f967baafdb92b7bfc42499e9fb5
TheLostTree <65834918+TheLostTree@users.noreply.github.com>
2023-04-13 13:28:26 -0700

    add some proto tools

---
+++ tools/index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+
+
+const proto_path = `no`;
+/**
+ * assuming directory structure is like this:
+ * /raw
+ * /translated
+ *    /protos
+ *    packetIds.json 
+ */
+
+function createOpcodeCsFile(){
+    let str = `namespace Common;\n`
+    str += `public enum Opcode {\n`
+    str += `    None = 0,\n`
+
+    const packetIds = JSON.parse(fs.readFileSync(`${proto_path}/translated/packetIds.json`));
+    for (const key in packetIds) {
+        str += `    ${packetIds[key]} = ${key},\n`
+    }
+    str += `}\n`
+    fs.writeFileSync(`./Common/Opcode.cs`, str);
+}
+
+function copyProtos(){
+    const protolist = [
+        // "PacketHead.proto",
+        "GetPlayerTokenReq.proto",
+        "GetPlayerTokenRsp.proto",
+        // "AvatarDataNotify.proto",
+        // "AvatarFightPropNotify.proto",
+        // "AvatarFightPropUpdateNotify.proto",
+        // "CombatInvocationsNotify.proto",
+        // "PlayerEnterSceneInfoNotify.proto",
+        // "UnionCmdNotify.proto",
+        // "AbilityInvocationsNotify.proto",
+        // "SceneTeamUpdateNotify.proto",
+        // "SceneEntityDisappearNotify.proto",
+        // "SceneEntityAppearNotify.proto",
+        // "PlayerEnterSceneNotify.proto",    
+        // "BattlePassAllDataNotify.proto",
+        // "AntiAddictNotify.proto",
+    ]
+
+    const includes = []
+
+    const proto_source = `${proto_path}/translated/protos`
+    //copy dependencies + protolist to ./protos
+
+    while(protolist.length > 0){
+        const proto = protolist.shift()
+        includes.push(proto)
+        const proto_path = `${proto_source}/${proto}`
+        const proto_content = fs.readFileSync(proto_path, 'utf8')
+        const proto_imports = proto_content.match(/import\s+"(.*)";/g)
+        if(proto_imports){
+            proto_imports.forEach(proto_import => {
+                const import_proto = proto_import.match(/import\s+"(.*)";/)[1]
+                if(!includes.includes(import_proto)){
+                    protolist.push(import_proto)
+                }
+            })
+        }
+    }
+    //copy dependencies + protolist to ./protos
+    includes.forEach(proto => {
+        fs.writeFileSync("./Common/Proto/"+proto, fs.readFileSync(`${proto_source}/${proto}`, 'utf8'))
+    })
+}
+
+
+// createOpcodeCsFile();
+copyProtos();
