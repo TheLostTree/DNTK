0ba84c8a40c6062de5f2de580d50404b960cc7be
TheLostTree <65834918+TheLostTree@users.noreply.github.com>
2023-04-13 13:09:11 -0700

    remove something silly

--- DPSMeter/DPSMeter/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DPSMeter.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- DPSMeter/DPSMeter/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace DPSMeter.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
